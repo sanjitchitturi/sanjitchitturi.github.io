28e907b30a2a6a6a572d6d030951a8bede965b14
sanjitchitturi <sanjit.chitturi@gmail.com>
2024-09-06 17:20:46 +0530

    Add files via upload

---
+++ New Webiste/app.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dynamicWord = document.getElementById('dynamic-word');
+    const words = ['Optimized.', 'Efficient.', 'Reinvented.', 'Seamless.'];
+    let index = 0;
+
+    function changeWord() {
+        dynamicWord.style.opacity = 0; 
+        setTimeout(() => {
+            index = (index + 1) % words.length;
+            dynamicWord.textContent = words[index];
+            dynamicWord.style.opacity = 1; 
+        }, 500); 
+
+        setTimeout(changeWord, 3000); 
+    }
+
+    changeWord();
+});
+
+// JavaScript for Header Scroll Hide
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('hidden');
+    } else {
+        header.classList.remove('hidden');
+    }
+});
